c468079e29c41898883f6ee7db4d61231c645b42
Robert Lehmann <robert.lehmann@sitec-systems.de>
2017-01-25 15:27:06 +0100

    srecovery: Update srecovery script
    Change the flash mechanism. The script now uses flashubi to flash a rootFS which will be extracted
    from a tar.bzip2 image file.
    The script changes the flashing behaviour of B1 LED to signalise the user that a recovery operation
    is running on the system
    
    Ticket #1656
    Ticket #1655

--- recipes-core/srecovery/files/srecovery.sh
+++ recipes-core/srecovery/files/srecovery.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Recovery script for sitec systems S4 device
-# Copyright (C) 2016  Robert Lehmann <robert.lehmann@sitec-systems.de>
+# Copyright (C) 2017  Robert Lehmann <robert.lehmann@sitec-systems.de>
 
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 RECOVERY_FILE="/var/ftk/recovery"
-TMP_IMG="/tmp/image"
 
 case "$1" in
 	start)
@@ -27,9 +26,9 @@ case "$1" in
 		fi
 
 		logger "$0 Starting Recovery"
+		echo timer > /sys/class/leds/B1/trigger
 		rm $RECOVERY_FILE
-		gunzip -c "/var/ftk/image.ubifs.gz" > $TMP_IMG
-		ubiupdatevol /dev/ubi0_1 /tmp/image
+		flashubi /dev/ubi0_1 /var/ftk/image.tar.bz2
 		fw_setenv root_alt 0
 		reboot
 		;;
